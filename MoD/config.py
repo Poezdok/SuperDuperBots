34dfebd1076904997c11f9ee6171389f6907c970
Alexey Konstantinov <poezd8365@gmail.com>
2021-11-14 20:35:13 +0300

    Add subject selection and I'm lucky button

--- MoD/config.py
+++ MoD/config.py
@@ -1,38 +1,57 @@
 
 token = '2124450938:AAHiV-rfqRo6NAgFgGoLAPM1mCHamgR84YE'
 
-start_answer = "Start text"
+#
+# start_answer = "Start text"
 
-database = [{
-    'Category 1': [{
-        'Subcategory 1': 'Answer 1',
-        'Subcategory 2': 'Answer 2',
-        'Subcategory 3': 'Answer 3',
-        'Subcategory 4': 'Answer 4'
-    }, "Category 1"],
-    'Category 2': [{
-        'Subcategory 1': 'Answer 1',
-        'Subcategory 2': 'Answer 2',
-        'Subcategory 3': 'Answer 3',
-        'Subcategory 4': 'Answer 4',
-    }, "Category 2"],
-}, start_answer]
+# database = [{
+#     'Category 1': [{
+#         'Subcategory 1': 'Answer 1',
+#         'Subcategory 2': 'Answer 2',
+#         'Subcategory 3': 'Answer 3',
+#         'Subcategory 4': 'Answer 4'
+#     }, "Category 1"],
+#     'Category 2': [{
+#         'Subcategory 1': 'Answer 1',
+#         'Subcategory 2': 'Answer 2',
+#         'Subcategory 3': 'Answer 3',
+#         'Subcategory 4': 'Answer 4',
+#     }, "Category 2"],
+# }, start_answer]
 
 # ---------------------------------------------------
 
-answer_1 = "Пачка тем 1"
-answer_2 = "Пачка тем 2"
-answer_3 = "Пачка тем3"
-
-answer = "Выбери категорию, чтобы получить пачку тем"
-
-category = {
-
-
-}
-
-# data = [{
-#     'Category 1' :
+# answer_1 = "Пачка тем 1"
+# answer_2 = "Пачка тем 2"
+# answer_3 = "Пачка тем3"
+#
+# answer = "Выбери категорию, чтобы получить пачку тем"
+#
+# category = {
 #
 #
 # }
+
+
+# database = [{}, start_answer]
+# #
+# subtree_1 = [
+#     {},
+#     "Описание к категории 1"
+# ]
+#
+# subsubtree_1 = {
+#     'Вариант 1': 'Ответ для варианта 1 из категории 1',
+#     'Вариант 2': 'Ответ для варианта 2 из категории 1',
+#     'Вариант 3': 'Ответ для варианта 3 из категории 1',
+#     'Вариант 4': 'Ответ для варианта 4 из категории 1',
+# }
+#
+# subtree_1[0].update(subsubtree_1)
+#
+# category_1 = {"Категория 1": subtree_1}
+#
+# database[0].update(category_1)
+#
+
+
--- MoD/main.py
+++ MoD/main.py
@@ -1,19 +1,20 @@
+
+# Импортируем библиотеки для работы с ботом, а также файлы
+
 import telebot
 import config
 from telebot import types
 import database
+import subjects
 
+
+# Создаём объект бот - привязываем его к нашему тг боту через токен
 tele_bot = telebot.TeleBot(config.token)
 
-global_state = []
 
-
-def get_state(id):
-    return global_state
-
-
+# Получаем локальное поддерево кнопок и ответов, на основе текущего "положения" собеседника
 def get_local_database(state):
-    temp = config.database
+    temp = subjects.database
     for key in state:
         temp = temp[0][key]
 
@@ -25,6 +26,7 @@ def get_reply(state, text):
     temp = get_local_database(state)
     # print(temp)
     markup = types.ReplyKeyboardMarkup()
+
     try:
         keys = temp[0].keys()
         answer = temp[1]
@@ -36,7 +38,7 @@ def get_reply(state, text):
         answer = temp
 
     if(len(state) > 0):
-        back = types.KeyboardButton('Back')
+        back = types.KeyboardButton('Назад')
         markup.row(back)
     return markup, answer
 
@@ -64,24 +66,30 @@ def text_message(message):
     print("State: ")
     print(state)
     print("")
+
+    if message.text == subjects.i_am_lucky_caption and len(state) == 0:
+        answer = subjects.i_am_lucky()
+        tele_bot.send_message(message.chat.id, answer)
+        return
+
     if check_text(state, message.text):
         state.append(message.text)
         print(state)
         database.save_profile(message.chat.id, state)
-
     else:
-        if message.text == "Back" and len(state) > 0:
+        if message.text == "Назад" and len(state) > 0:
             state.pop()
             database.save_profile(message.chat.id, state)
         else:
             print("There is no such key")
-            if(len(state) > 0):
-                tele_bot.send_message(message.chat.id, "Choose button", )
+            if len(state) > 0:
+                tele_bot.send_message(message.chat.id, "Выбери кнопку!", )
 
                 return
+
     markup, answer = get_reply(state, message.text)
     if len(answer) == 0:
-        Answer = config.start_answer
+        answer = subjects.start_answer
 
     tele_bot.send_message(message.chat.id, answer, reply_markup=markup)
 
